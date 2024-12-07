cc215d188fde321a9cc33ee35e9d049f1f2f5e8f
Paul Johnston <pcj@stack.build>
2024-12-07 11:06:26 -0700

    Refactor tests to improve clarity and update endpoint descriptions; adjust TypeScript configuration to include all test files

--- projects/worker-api/test/index.spec.ts
+++ projects/worker-api/test/index.spec.ts
@@ -2,15 +2,9 @@
 import { env, createExecutionContext, waitOnExecutionContext, SELF } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 import worker from '../src/index';
-import { IncomingRequestCfProperties } from '@cloudflare/workers-types/experimental';
-
-// For now, you'll need to do something like this to get a correctly-typed
-// `Request` to pass to `worker.fetch()`.
-// const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
-
 
 describe('Worker', () => {
-    it('responds with Hello World! (unit style)', async () => {
+    it('responds OK from /healthz endpoint (unit style)', async () => {
         const request = new Request('https://api.vsx.dev/healthz');
         // Create an empty context to pass to `worker.fetch()`.
         const ctx = createExecutionContext();
@@ -20,7 +14,7 @@ describe('Worker', () => {
         expect(await response.text()).toMatchInlineSnapshot(`"OK"`);
     });
 
-    it('responds with Hello World! (integration style)', async () => {
+    it('responds OK from /healthz endpoint (integration style)', async () => {
         const response = await SELF.fetch('https://api.vsx.dev/healthz');
         expect(await response.text()).toMatchInlineSnapshot(`"OK"`);
     });
